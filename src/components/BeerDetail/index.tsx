7f25df41e9e4cc85a5e302127c88e2a0808006d8
Wen Barbosa <wenblack012@gmail.com>
2023-09-18 17:17:16 -0300

    Created reviewView

--- src/components/BeerDetail/index.tsx
+++ src/components/BeerDetail/index.tsx
@@ -17,6 +17,7 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import Link from 'next/link';
 
 export const BeerDetail = (
   { name, ABV, IBU, Notes, categorie, categorieId, description, id, imageUrl, rating, reviews }: BeerProps
@@ -88,12 +89,129 @@ export const BeerDetail = (
       <main className='flex flex-col items-center w-screen  justify-center'>
         <div className=" bg-white  flex flex-col w-full mr-4  rounded-lg p-4 max-w-sm ">
           <div className='flex justify-start '>
-            <a href='/dashboard' className=' flex  rounded-md font-bold text-xs bg-transparent items-center text-white justify-center'>
+            <Link href='/dashboard' className=' flex  rounded-md font-bold text-xs bg-transparent items-center text-white justify-center'>
               <Image src={ArrowLeft} alt='Arrow left icon' />
-            </a>
+            </Link>
           </div>
           <div className='flex justify-end mb-4'>
             <span className='px-2 py-1 flex  rounded-md font-bold text-xs bg-[#15803d] items-center text-white justify-center'>
+              {avg}
+            </span>
+          </div>
+          <span className='flexflex-col items-center justify-center'>
+            <Image
+              src={image}
+              width={150}
+              height={150}
+              alt={`${name} Product`}
+              className="h-40  p-4 flex shadow-lg object-cover m-auto  w-28 rounded-md "
+            />
+            <div className="flex items-center justify-center">
+              {renderStars(calculateAverageRating())}
+            </div>
+          </span>
+
+          <div className="mt-2  border-t-2 ">
+            <span className='flex items-center justify-between'>
+              <h3 className=" text-gray capitalize font-semibold">
+                {name}
+              </h3>
+            </span>
+            <span className=" px-2 py-1  items-center bg-button text-white font-semibold text-xs rounded-md">
+              {family}
+            </span>
+          </div>
+        </div>
+        <Tabs defaultValue="account" className='w-screen-md ' >
+          <TabsList className="grid w-screen max-w-md  grid-cols-2">
+            <TabsTrigger value="account">Informações</TabsTrigger>
+            <TabsTrigger value="password">Comentários</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            <Card className='grid grid-cols-2 gap-3 p-8 w-screen max-w-md '>
+              <span>
+                <CardTitle>IBU</CardTitle>
+                <CardDescription>
+                  {IBU}
+                </CardDescription>
+              </span>
+              <span>
+                <CardTitle>ABV</CardTitle>
+                <CardDescription>
+                  {ABV} %
+                </CardDescription>
+              </span>
+              <span>
+                <CardTitle>Nota</CardTitle>
+                <CardDescription>
+                  {rating}/5
+                </CardDescription>
+              </span>
+              <span>
+                <CardTitle>Família</CardTitle>
+                <CardDescription>
+                  {family}
+                </CardDescription>
+              </span>
+              <span>
+                <CardTitle>Ingredientes</CardTitle>
+                <CardDescription className='capitalize'>
+                  {description}
+                </CardDescription>
+              </span>
+              <span>
+                <CardTitle>Reviews</CardTitle>
+                <CardDescription>
+                  {reviews?.length}
+                </CardDescription>
+              </span>
+            </Card>
+          </TabsContent>
+          <TabsContent value="password">
+            <Card className='pb-4'>
+              <CardHeader >
+                <CardTitle>Sua opinião</CardTitle>
+                <CardDescription>
+                  Conte-nos o que você achou dessa breja:
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <textarea className='border px-2 w-full border-shadow' rows={5}></textarea>
+                </div>
+              </CardContent>
+              <CardFooter className='flex w-full items-center justify-center'>
+                <Button type='submit' value='Salvar'></Button>
+              </CardFooter>
+              <ul className='flex flex-col gap-2 mt-4 border-t pt-4'>
+                {reviews?.map((review) =>
+                  <li className=' flex '>
+                    <span className='px-8'>
+                      <h3 className='font-semibold'>{review.Title}</h3>
+                      <p className='text-muted-foreground'>{review.content}</p>
+                    </span>
+                  </li>
+                )}
+              </ul>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </main>
+    );
+  }
+
+
+  if (avg === "BOA") {
+    return (
+      <main className='flex flex-col items-center w-screen  justify-center'>
+        <div className=" bg-white  flex flex-col w-full mr-4  rounded-lg p-4 max-w-sm ">
+          <div className='flex justify-start '>
+            <Link href='/dashboard' className=' flex  rounded-md font-bold text-xs bg-transparent items-center text-white justify-center'>
+              <Image src={ArrowLeft} alt='Arrow left icon' />
+            </Link>
+          </div>
+          <div className='flex justify-end mb-4'>
+            <span className='px-2 py-1 flex  rounded-md font-bold text-xs bg-[#f59e0b] items-center text-white justify-center'>
               {avg}
             </span>
           </div>
@@ -190,98 +308,13 @@ export const BeerDetail = (
   }
 
 
-  if (avg === "BOA") {
-    return (
-      <main className='flex flex-col items-center w-screen  justify-center'>
-        <div className=" bg-white  flex flex-col w-full mr-4  rounded-lg p-4 max-w-sm ">
-          <div className='flex justify-start '>
-            <a href='/dashboard' className=' flex  rounded-md font-bold text-xs bg-transparent items-center text-white justify-center'>
-              <Image src={ArrowLeft} alt='Arrow left icon' />
-            </a>
-          </div>
-          <div className='flex justify-end mb-4'>
-            <span className='px-2 py-1 flex  rounded-md font-bold text-xs bg-[#f59e0b] items-center text-white justify-center'>
-              {avg}
-            </span>
-          </div>
-          <span className='flexflex-col items-center justify-center'>
-            <Image
-              src={image}
-              width={150}
-              height={150}
-              alt={`${name} Product`}
-              className="h-40  p-4 flex shadow-lg object-cover m-auto  w-28 rounded-md "
-            />
-            <div className="flex items-center justify-center">
-              {renderStars(calculateAverageRating())}
-            </div>
-          </span>
-
-          <div className="mt-2  border-t-2 ">
-            <span className='flex items-center justify-between'>
-              <h3 className=" text-gray capitalize font-semibold">
-                {name}
-              </h3>
-            </span>
-            <span className=" px-2 py-1  items-center bg-button text-white font-semibold text-xs rounded-md">
-              {family}
-            </span>
-          </div>
-        </div>
-        <Tabs defaultValue="account" className='max-w-screen-md ' >
-          <TabsList className="grid  grid-cols-2">
-            <TabsTrigger value="account">Informações</TabsTrigger>
-            <TabsTrigger value="password">Comentários</TabsTrigger>
-          </TabsList>
-          <TabsContent value="account">
-            <Card>
-              <CardHeader>
-                <CardTitle>Informações</CardTitle>
-                <CardDescription>
-                  Make changes to your account here. Click save when you're done.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <p>The info about beer</p>
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button value='Save'></Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-          <TabsContent value="password">
-            <Card>
-              <CardHeader>
-                <CardTitle>Comentários</CardTitle>
-                <CardDescription>
-                  Change your password here. After saving, you'll be logged out.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <p>That comments about beer</p>
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button value='save'></Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-        </Tabs>
-      </main>
-    );
-  }
-
-
   return (
     <main className='flex flex-col items-center w-screen  justify-center'>
       <div className=" bg-white  flex flex-col w-full mr-4  rounded-lg p-4 max-w-sm ">
         <div className='flex justify-start '>
-          <a href='/dashboard' className=' flex  rounded-md font-bold text-xs bg-transparent items-center text-white justify-center'>
+          <Link href='/dashboard' className=' flex  rounded-md font-bold text-xs bg-transparent items-center text-white justify-center'>
             <Image src={ArrowLeft} alt='Arrow left icon' />
-          </a>
+          </Link>
         </div>
         <div className='flex justify-end mb-4'>
           <span className='px-2 py-1 flex  rounded-md font-bold text-xs bg-[#dc2626] items-center text-white justify-center'>
@@ -312,45 +345,77 @@ export const BeerDetail = (
           </span>
         </div>
       </div>
-      <Tabs defaultValue="account" className='max-w-screen-md ' >
-        <TabsList className="grid  grid-cols-2">
+      <Tabs defaultValue="account" className='w-screen-md ' >
+        <TabsList className="grid w-screen max-w-md  grid-cols-2">
           <TabsTrigger value="account">Informações</TabsTrigger>
           <TabsTrigger value="password">Comentários</TabsTrigger>
         </TabsList>
         <TabsContent value="account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Informações</CardTitle>
+          <Card className='grid grid-cols-2 gap-3 p-8 w-screen max-w-md '>
+            <span>
+              <CardTitle>IBU</CardTitle>
               <CardDescription>
-                Make changes to your account here. Click save when you're done.
+                {IBU}
               </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <p>The info about beer</p>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button value='Save'></Button>
-            </CardFooter>
+            </span>
+            <span>
+              <CardTitle>ABV</CardTitle>
+              <CardDescription>
+                {ABV} %
+              </CardDescription>
+            </span>
+            <span>
+              <CardTitle>Nota</CardTitle>
+              <CardDescription>
+                {rating}/5
+              </CardDescription>
+            </span>
+            <span>
+              <CardTitle>Família</CardTitle>
+              <CardDescription>
+                {family}
+              </CardDescription>
+            </span>
+            <span>
+              <CardTitle>Ingredientes</CardTitle>
+              <CardDescription className='capitalize'>
+                {description}
+              </CardDescription>
+            </span>
+            <span>
+              <CardTitle>Reviews</CardTitle>
+              <CardDescription>
+                {reviews?.length}
+              </CardDescription>
+            </span>
           </Card>
         </TabsContent>
         <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Comentários</CardTitle>
+          <Card className='pb-4'>
+            <CardHeader >
+              <CardTitle>Sua opinião</CardTitle>
               <CardDescription>
-                Change your password here. After saving, you'll be logged out.
+                Conte-nos o que você achou dessa breja:
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
               <div className="space-y-1">
-                <p>That comments about beer</p>
+                <textarea className='border px-2 w-full border-shadow' rows={5}></textarea>
               </div>
             </CardContent>
-            <CardFooter>
-              <Button value='save'></Button>
+            <CardFooter className='flex w-full items-center justify-center'>
+              <Button type='submit' value='Salvar'></Button>
             </CardFooter>
+            <ul className='flex flex-col gap-2 mt-4 border-t pt-4'>
+              {reviews?.map((review) =>
+                <li className=' flex '>
+                  <span className='px-8'>
+                    <h3 className='font-semibold'>{review.Title}</h3>
+                    <p className='text-muted-foreground'>{review.content}</p>
+                  </span>
+                </li>
+              )}
+            </ul>
           </Card>
         </TabsContent>
       </Tabs>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ import { ButtonHTMLAttributes, MouseEventHandler } from "react"
 
 
 interface ButtonProps {
-    type?: "button" | "submit"   
+    type?: "button" | "submit"
     number?: number
     categorie?: boolean
     value: string
@@ -16,7 +16,7 @@ const poppins = Poppins({ subsets: ['latin'], weight: ['500', '400', '700', '600
 export function Button({ onclick, type, number, categorie, value }: ButtonProps) {
     if (categorie) {
         return (
-            <button className={`flex hover:opacity-80 h-12 w-[215px] items-center  bg-bgbutton gap-3 rounded-lg justify-center py-[14px] bg-button ${poppins.className}`}>
+            <button className={`flex hover:opacity-80 h-12 w-[215px] text-white items-center  bg-bgbutton gap-3 rounded-lg justify-center py-[14px] bg-button ${poppins.className}`}>
                 <span>
                     {value} ({number})
                 </span>
@@ -24,7 +24,7 @@ export function Button({ onclick, type, number, categorie, value }: ButtonProps)
         )
     }
     return (
-        <button onClick={onclick} type={type} className={`flex  h-12 hover:opacity-80 w-[80vw]  max-w-[348px]  items-center bg-bgbutton gap-3 rounded-lg justify-center py-[14px] bg-button ${poppins.className}`}>
+        <button onClick={onclick} type={type} className={`flex text-white  h-12 hover:opacity-80 w-[80vw]  max-w-[348px]  items-center bg-bgbutton gap-3 rounded-lg justify-center py-[14px] bg-button ${poppins.className}`}>
             <span>
                 {value}
             </span>
